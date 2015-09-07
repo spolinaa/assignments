68f7cfafc59434adc6cea363480a8988617197b9
Podkopaev Anton <podkoav239@gmail.com>
2015-09-07 14:57:27 +0300

    Added test project.

---
+++ B/src/main/kotlin/ru/spbu/test/main.kt
@@ -0,0 +1,9 @@
+package ru.spbu.test
+
+fun hello(): String {
+  return "hello"
+}
+
+fun main(args: Array<String>) {
+  print("Hello world!")
+}
---
+++ B/src/test/kotlin/ru/spbu/test/main.kt
@@ -0,0 +1,10 @@
+package ru.spbu.test
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class TestClass {
+  Test fun helloTest() {
+    assertEquals("hello", hello())
+  }
+}
