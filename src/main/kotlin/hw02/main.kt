780854c8b42d5f650791a8719697d84179c0ffc9
spolinaa <sokolova.polina@mail.ru>
2015-09-25 00:56:41 +0300

    Removed git add --a, brackets, renamed c to computer

--- src/main/kotlin/hw02/main.kt
+++ src/main/kotlin/hw02/main.kt
@@ -23,7 +23,7 @@ class Computer(val os : String, val infected : Boolean, val number : Int) {
     var i = infected
     fun isInfected() : Boolean = i
     fun infection(v : Double) : Unit {
-        if ((!i) && (v < probability(os))) { i = true }
+        if (!i && (v < probability(os))) { i = true }
     }
     fun state() : String {
         if (this.isInfected()) { return "+" }
@@ -34,17 +34,17 @@ class Computer(val os : String, val infected : Boolean, val number : Int) {
 class LocalNetwork(val os : Array<String>, val connected : Array<Array<Boolean>>,
                    val infection : Array<Boolean>) : ArrayGraph(connected) {
     var count = 0
-    var c = arrayOfNulls(infection.size()) : Array<Computer?>
+    var computer = arrayOfNulls(infection.size()) : Array<Computer?>
     fun letsInfect(random : Double) {
         val n = infection.size() - 1
         for (i in 0..n) {
-            c[i] = Computer(os[i], infection[i], i)
+            computer[i] = Computer(os[i], infection[i], i)
         }
         for (i in 0..n) {
-            if (c[i]!!.isInfected()) {
+            if (computer[i]?.isInfected() == true) {
                 for (k in 0..n) {
                     if (connected[i][k]) {
-                        c[k]!!.infection(random)
+                        computer[k]?.infection(random)
                     }
                 }
             }
@@ -54,29 +54,29 @@ class LocalNetwork(val os : Array<String>, val connected : Array<Array<Boolean>>
 
     fun smallStatus() : kotlin.String {
         var res = ""
-        for (i in 0..os.size() - 1) { res += c[i]!!.state()}
+        for (i in 0..os.size() - 1) { res += computer[i]!!.state()}
         return res
     }
 
     fun status() {
         print("\n\n       %A.MyNetwork:\n $count")
         print("  OS X (0)")
-        print(c[0]!!.state())
+        print(computer[0]!!.state())
         print("  -  OS X (1)")
-        print(c[1]!!.state())
+        print(computer[1]!!.state())
         print("\n                   | ")
         print(" Linux (3)")
-        print(c[3]!!.state())
+        print(computer[3]!!.state())
         print("  -  Linux (4)")
-        print(c[4]!!.state())
+        print(computer[4]!!.state())
         print("\n    |              |")
         print("Windows (5)")
-        print(c[5]!!.state())
+        print(computer[5]!!.state())
         print(" -  OS X (2)")
-        print(c[2]!!.state())
+        print(computer[2]!!.state())
         print("\n             |")
         print("       Windows (6)")
-        print(c[6]!!.state())
+        print(computer[6]!!.state())
         print("\n")
     }
 }
