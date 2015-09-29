b9e9b775dae4f76b4241497915482d0385538704
spolinaa <sokolova.polina@mail.ru>
2015-09-30 00:13:47 +0300

    Removed some if & when, change balanceFactor & height

--- src/main/kotlin/hw03/main.kt
+++ src/main/kotlin/hw03/main.kt
@@ -1,45 +1,26 @@
 /* AVL trees
-(expectation: 3 h; reality: 10 h)
+(expectation: 3 h; reality: 11 h)
 by Sokolova Polina */
 
 package hw03
 
 abstract class Tree {}
 class Empty() : Tree() {}
-class Leaf(val value: Int): Tree() {}
 class Node(val value: Int, val l: Tree, val r: Tree): Tree() {}
 
-fun Tree.height(acc : Int) : Int {
-    when (this) {
-        is Empty -> { return acc }
-        is Node  -> {
-            val lHeight = l.height(acc + 1)
-            val rHeight = r.height(acc + 1)
-            if (lHeight > rHeight) { return lHeight }
-            else { return rHeight }
-        }
-    }
-    return acc
-}
+fun Tree.height() : Int = if (this is Node) (1 + Math.max(l.height(), r.height())) else 0
 
-fun Tree.balanceFactor() : Int {
-    when (this) {
-        is Empty -> { return 0 }
-        is Node  -> { return (l.height(0) - r.height(0)) }
-    }
-    return 0
-}
+fun Tree.balanceFactor() : Int = if (this is Node) (l.height() - r.height()) else 0
 
 fun Tree.treeToString() : String {
     when (this) {
-        is Empty -> { return "Empty" }
         is Node  -> {
             val lRes = l.treeToString()
             val rRes = r.treeToString()
             return "Node($value, $lRes, $rRes)"
         }
+        else -> { return "Empty" }
     }
-    return "Empty"
 }
 
 fun Tree.smallLeftRotation() : Tree {
@@ -48,17 +29,13 @@ fun Tree.smallLeftRotation() : Tree {
     var rValue : Int = 0
     var lR : Tree = Empty()
     var rR : Tree = Empty()
-    when (this) {
-        is Node -> {
-            cValue = value
-            L = l
-            when (r) {
-                is Node -> {
-                    rValue = r.value
-                    lR = r.l
-                    rR = r.r
-                }
-            }
+    if (this is Node) {
+        cValue = value
+        L = l
+        if (r is Node) {
+            rValue = r.value
+            lR = r.l
+            rR = r.r
         }
     }
     return Node(rValue, Node(cValue, L, lR), rR)
@@ -70,46 +47,39 @@ fun Tree.smallRightRotation() : Tree {
     var lValue : Int = 0
     var lL : Tree = Empty()
     var rL : Tree = Empty()
-    when (this) {
-        is Node -> {
-            R = r
-            cValue = value
-            when (l) {
-                is Node -> {
-                    lValue = l.value
-                    lL = l.l
-                    rL = l.r
-                }
-            }
+    if (this is Node) {
+        R = r
+        cValue = value
+        if (l is Node) {
+            lValue = l.value
+            lL = l.l
+            rL = l.r
         }
     }
     return Node(lValue, lL, Node(cValue, rL, R))
 }
 
 fun Tree.balance() : Tree {
-    when (this) {
-        is Empty -> { return Empty() }
-        is Node  -> {
-            val balance = this.balanceFactor()
-            if (balance == -2) {
-                if (this.r.balanceFactor() <= 0) {
-                    return this.smallLeftRotation()
-                }
-                else {
+    if (this is Node) {
+        val balance = this.balanceFactor()
+        if (balance == -2) {
+            when {
+                this.r.balanceFactor() > 0 -> {
                     val right = r.smallRightRotation()
                     val tree = Node(value, l, right)
                     return tree.smallLeftRotation()
                 }
+                else -> { return this.smallLeftRotation() }
             }
-            if (balance == 2) {
-                if (this.l.balanceFactor() >= 0) {
-                    return this.smallRightRotation()
-                }
-                else {
+        }
+        if (balance == 2) {
+            when {
+                this.l.balanceFactor() < 0 -> {
                     val left = l.smallLeftRotation()
                     val tree = Node(value, left, r)
                     return tree.smallRightRotation()
                 }
+                else -> { return this.smallRightRotation() }
             }
         }
     }
@@ -121,9 +91,11 @@ fun Tree.insert(a : Int) : Tree {
         when (this) {
             is Empty -> { return Node(a, Empty(), Empty()) }
             is Node -> {
-                if (a < value) { return Node(value, l.add(), r) }
-                else if (a == value) { return this }
-                else { return Node(value, l, r.add()) }
+                when {
+                    a < value -> { return Node(value, l.add(), r) }
+                    a > value -> { return Node(value, l, r.add()) }
+                    else -> { return this }
+                }
             }
         }
         return Empty()
@@ -133,29 +105,21 @@ fun Tree.insert(a : Int) : Tree {
 }
 
 fun Tree.minInRight() : Int {
-    when (this) {
-        is Node  -> {
-            when (l) {
-                is Empty -> { return value }
-                is Node  -> { return l.minInRight() }
-            }
+    if (this is Node) {
+        when (l) {
+            is Empty -> { return value }
+            is Node  -> { return l.minInRight() }
         }
     }
     return 0
 }
 
 fun Tree.replace(a : Int, b : Int) : Tree {
-    when (this) {
-        is Empty -> { return Empty() }
-        is Node  -> {
-            if (a == value) {
-                return Node(b, l, r) }
-            else if (a > value) {
-                return Node(value, l, r.replace(a, b))
-            }
-            else {
-                return Node(value, l.replace(a, b), r)
-            }
+    if (this is Node) {
+        when {
+            a < value -> { return Node(value, l.replace(a, b), r) }
+            a > value -> { return Node(value, l, r.replace(a, b)) }
+            else -> { return Node(b, l, r) }
         }
     }
     return Empty()
@@ -163,14 +127,11 @@ fun Tree.replace(a : Int, b : Int) : Tree {
 
 fun Tree.remove(a : Int) : Tree {
     fun Tree.delete() : Tree {
-        when (this) {
-            is Empty -> { return Empty() }
-            is Node -> {
-                if (a < value) {
-                    return Node(value, l.delete(), r).balance() }
-                else if (a > value) {
-                    return Node(value, l, r.delete()).balance() }
-                else if (a == value) {
+        if (this is Node) {
+            when {
+                a < value -> { return Node(value, l.delete(), r).balance() }
+                a > value -> { return Node(value, l, r.delete()).balance() }
+                else -> {
                     when (r) {
                         is Empty -> { return l }
                         is Node -> {
@@ -188,14 +149,8 @@ fun Tree.remove(a : Int) : Tree {
 }
 
 fun Tree.search(a : Int) : Boolean {
-    when (this) {
-        is Node  -> {
-            if (a == value) { return true }
-            else {
-                if (l.search(a) || r.search(a)) { return true }
-                else { return false }
-            }
-        }
+    if (this is Node) {
+        return (a == value) || l.search(a) || r.search(a)
     }
     return false
 }
@@ -216,7 +171,4 @@ fun Tree.toText(): List<String> {
         val vText = listOf("$value\n")
         lText + vText + rText
     }
-}
-
-fun main(args : Array<String>) {
 }
