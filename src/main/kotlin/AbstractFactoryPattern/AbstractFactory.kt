090fb3b229575a36cc583e22803fd9fcefb4460e
Mikhail Wall <mikhwall@gmail.com>
2015-09-30 17:48:07 +0300

    updated according to remarks

--- src/main/kotlin/AbstractFactoryPattern/AbstractFactory.kt
+++
@@ -1,9 +0,0 @@
-package AbstractFactoryPattern
-
-/**
- * Created by Mikhail on 18.09.2015.
- */
-public abstract class AbstractFactory {
-    abstract fun getColor(color: String): Color?
-    abstract fun getShape(shape: String): Shape?
-}
--- src/main/kotlin/AbstractFactoryPattern/Color.kt
+++
@@ -1,8 +0,0 @@
-package AbstractFactoryPattern
-
-/**
- * Created by Mikhail on 18.09.2015.
- */
-public interface Color {
-    public fun fill()
-}
