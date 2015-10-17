4f8b4d9fc559cb0592ef676eb58de9e976cdcc25
spolinaa <sokolova.polina@mail.ru>
2015-10-17 22:45:50 +0300

    Added threads in merge sort, updated results

--- src/main/kotlin/hw05/main.kt
+++ src/main/kotlin/hw05/main.kt
@@ -15,10 +15,9 @@ public fun increment (a : Int, b : Int, threadNumber : Int) : Int {
 
 public fun mergeSort(a : Array<Int>, threadNumber: Int) : Array<Int> {
     //val startTime = System.currentTimeMillis();
-    if (threadNumber == 0) { return a }
     val maxThreadNumber = Math.min(a.size(), threadNumber) - 1
-    val currentThread = 0
-    var threadArray = Array(maxThreadNumber + 1, { })
+    val currentThread = 1
+    var threadArray : Array<Thread> = Array(maxThreadNumber + 1, {_ -> Thread() })
     fun merge(a : Array<Int>, start : Int, middle : Int, end : Int) {
         var leftArray = a.copyOfRange(start, middle + 1);
         var rightArray = a.copyOfRange(middle + 1, end + 1);
@@ -41,30 +40,19 @@ public fun mergeSort(a : Array<Int>, threadNumber: Int) : Array<Int> {
         var res = a
         if (size == 1) { return }
         var middle = (start + end) / 2
-        var threadRemainder = maxThreadNumber - currentT
-        when {
-            threadRemainder >= 3 -> {
-                threadArray[currentT + 1] = sort(a, start, middle, currentT + 1)
-                threadArray[currentT + 2] = sort(a, middle + 1, end, currentT + 2)
-                threadArray[currentT + 3] = merge(res, start, middle, end)
-            }
-            threadRemainder == 2 -> {
-                threadArray[currentT + 1] = sort(a, start, middle, currentT + 1)
-                threadArray[currentT + 2] = sort(a, middle + 1, end, currentT + 2)
-                merge(res, start, middle, end)
-            }
-            threadRemainder == 1 -> {
-                threadArray[currentT + 1] = sort(a, start, middle, currentT + 1)
-                sort(a, middle + 1, end, currentT)
-                merge(res, start, middle, end)
-            }
-            else -> {
-                sort(a, start, middle, currentT)
-                sort(a, middle + 1, end, currentT)
-                merge(res, start, middle, end)}
+        if (maxThreadNumber > currentT) {
+            val t = Thread({sort(a, start, middle, currentT + 1)})
+            t.start()
+            sort(a, middle + 1, end, currentT + 2)
+            t.join()
         }
+        else {
+            sort(a, start, middle, currentT)
+            sort(a, middle + 1, end, currentT)
+        }
+        merge(res, start, middle, end)
     }
-    threadArray[currentThread] = sort(a, 0, a.size() - 1, currentThread)
+    sort(a, 0, a.size() - 1, currentThread)
     //val timeSpent = System.currentTimeMillis() - startTime
     //println("Size: ${a.size()}, Threads: $threadNumber, Time: $timeSpent мс")
     return a
@@ -74,4 +62,7 @@ public fun Array<Int>.arrayToString() : String {
     var res = ""
     for (i in 0..this.size() - 1) { res += "${this[i]} " }
     return res
+}
+
+fun main(args: Array<String>) {
 }
